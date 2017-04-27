ab6eacc1e794268c9a50615f90f61e876cd41a5e
vladimir <ulitochnik@i.ua>
2017-04-27 15:29:48 +0300

    1

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
     
     cout << "Hello, world!" << endl;
     cout << "Hello, Jenkins!" << endl;
-    cout << "Hello, Hell" << endl;
+    //cout << "Hello, Hell" << endl;
 
     return 0;
 }
