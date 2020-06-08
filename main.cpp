bc35c63ea5e044a64aecb12de21e400b711066cb
chumachenko-vy <ulitochnik@i.ua>
2020-06-08 19:32:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 int main(int argc, char** argv) 
 {
     
-    cout << "Hello, world! 123456789" << endl;
+    cout << "Hello, world! 123456789-6666" << endl;
 
     return 0;
 }
